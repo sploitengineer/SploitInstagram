5bf2a82dc64158074d5593d476d8e57df55f4faa
root <root@localhost.localdomain>
2020-04-13 10:09:01 +0530

    First commit

---
+++ lib/__init__.py
@@ -0,0 +1,2 @@
+# Date: 05/04/2020
+# Author: Kaushal
---
+++ lib/proxy.py
@@ -0,0 +1,26 @@
+# Date: 05/04/2020
+# Author: Kaushal
+# Description: Proxy
+
+
+class Proxy(object):
+
+    def __init__(self, proxy):
+        self.proxy = proxy
+
+    @property
+    def ip(self):
+        return self.proxy['ip']
+
+    @property
+    def port(self):
+        return self.proxy['port']
+
+    @property
+    def country(self):
+        return self.proxy['country']
+
+    @property
+    def addr(self):
+        addr = '{}:{}'.format(self.proxy['ip'], self.proxy['port'])
+        return {'http': addr, 'https': addr}
